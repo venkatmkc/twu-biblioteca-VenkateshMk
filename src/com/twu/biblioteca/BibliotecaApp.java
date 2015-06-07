6db064f258be4b1631a814d4eeab47642d044098
Venkatmkc <venkatmkc@gmail.com>
2015-06-07 15:22:08 +0530

    Added spec, main menu should be displayed

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,11 +1,14 @@
 package com.twu.biblioteca;
 
 public class BibliotecaApp {
-    public final String welcomeMessage = "Welcome to Biblioteca!";
+    private String welcomeMessage = "Welcome to Biblioteca!";
+    private String mainMenu;
     private Books books;
 
-    public BibliotecaApp(Books books) {
+    public BibliotecaApp(Books books, String welcomeMessage, String mainMenu) {
         this.books = books;
+        this.welcomeMessage = welcomeMessage;
+        this.mainMenu = mainMenu;
     }
 
     public void displayWelcomeMessage() {
@@ -14,5 +17,9 @@ public class BibliotecaApp {
 
     public void displayBookList() {
         System.out.print(books);
+    }
+
+    public void displayMainMenu() {
+        System.out.println(mainMenu);
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -11,6 +11,8 @@ import static org.mockito.Mockito.*;
 
 public class BibliotecaAppTest {
     private final ByteArrayOutputStream outputStreamContent = new ByteArrayOutputStream();
+    private String welcomeMessage = new String("Welcome to Biblioteca");
+    private String mainMenu = new String("List Books");
 
     @Mock
     Books books;
@@ -26,7 +28,7 @@ public class BibliotecaAppTest {
 
     @Test
     public void welcomeMessageShouldBeDisplayed() {
-        BibliotecaApp bibliotecaApp = new BibliotecaApp(books);
+        BibliotecaApp bibliotecaApp = new BibliotecaApp(books, welcomeMessage, mainMenu);
 
         bibliotecaApp.displayWelcomeMessage();
         String actualMessage = outputStreamContent.toString();
@@ -36,13 +38,23 @@ public class BibliotecaAppTest {
 
     @Test
     public void listOfBooksShouldBeDisplayed() {
-        BibliotecaApp bibliotecaApp = new BibliotecaApp(books);
+        BibliotecaApp bibliotecaApp = new BibliotecaApp(books, welcomeMessage, mainMenu);
 
         bibliotecaApp.displayBookList();
         String actualBookList = outputStreamContent.toString();
 
         assertThat(actualBookList, is(equalTo("Kite Runner         |Khaled Hosseini     |2003\n" +
                 "The Sky Is Falling  |Sidney Sheldon      |2001\n")));
+    }
+
+    @Test
+    public void mainMenuShouldBeDisplayed() {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp(books, welcomeMessage, mainMenu);
+
+        bibliotecaApp.displayMainMenu();
+        String actualMainMenu = outputStreamContent.toString();
+
+        assertThat(actualMainMenu, is(equalTo("List Books\n")));
     }
 
     @After
