af43f63fb6457fbe17db0981f2dd31ac0abe34fb
Venkatmkc <venkatmkc@gmail.com>
2015-06-07 16:19:18 +0530

    Refactored - changed name of input handler class to console input handler

--- src/com/twu/biblioteca/ConsoleInputHandler.java
+++ src/com/twu/biblioteca/ConsoleInputHandler.java
@@ -2,12 +2,13 @@ package com.twu.biblioteca;
 
 import java.util.Scanner;
 
-public class InputHandler {
+public class ConsoleInputHandler {
     private Scanner consoleInput;
 
-    public InputHandler(Scanner consoleInput) {
+    public ConsoleInputHandler(Scanner consoleInput) {
         this.consoleInput = consoleInput;
     }
+
     public String getMainMenuOptionFromUser() {
         return consoleInput.nextLine();
     }
--- test/com/twu/biblioteca/ConsoleInputHandlerTest.java
+++ test/com/twu/biblioteca/ConsoleInputHandlerTest.java
@@ -8,13 +8,13 @@ import java.util.Scanner;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class InputHandlerTest {
+public class ConsoleInputHandlerTest {
     @Test
     public void mainMenuOptionShouldBeObtained() {
         ByteArrayInputStream inputStreamContent = new ByteArrayInputStream("List Books".getBytes());
         System.setIn(inputStreamContent);
         Scanner consoleInput = new Scanner(inputStreamContent);
-        InputHandler inputHandler = new InputHandler(consoleInput);
+        ConsoleInputHandler inputHandler = new ConsoleInputHandler(consoleInput);
 
         String actualOption = inputHandler.getMainMenuOptionFromUser();
 
