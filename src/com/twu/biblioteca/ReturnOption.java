b712591bfea591629fb50f0d8e57e997ff6c0b3d
Venkatmkc <venkatmkc@gmail.com>
2015-06-14 01:32:06 +0530

    Added spec, user input should be obtained from the user for returning the book

---
+++ src/com/twu/biblioteca/ReturnOption.java
@@ -0,0 +1,20 @@
+package com.twu.biblioteca;
+
+import java.util.Stack;
+
+public class ReturnOption implements MainMenuAction{
+    private final ConsoleInputOutput consoleInputOutput;
+    private final BookParser bookParser;
+    private final Books books;
+
+    public ReturnOption(ConsoleInputOutput consoleInputOutput, BookParser bookParser, Books books) {
+        this.consoleInputOutput = consoleInputOutput;
+        this.bookParser = bookParser;
+        this.books = books;
+    }
+
+    @Override
+    public void obtainOptionResult() {
+        String userInput = consoleInputOutput.getUserInput();
+    }
+}
---
+++ test/com/twu/biblioteca/ReturnOptionTest.java
@@ -0,0 +1,42 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.HashMap;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class ReturnOptionTest {
+    @Mock
+    private ConsoleInputOutput consoleInputOutput;
+
+    @Mock
+    BookParser bookParser;
+
+    @Mock
+    Books books;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+    }
+
+    @Test
+    public void bookTitleShouldBeObtainedFromTheUser() {
+        HashMap<Book, Boolean> bookList = new HashMap<Book, Boolean>();
+        Book bookOne = new Book("Kite Runner", "Khaled Hosseini", "2003");
+        Book bookTwo = new Book("The Sky Is Falling", "Sidney Sheldon", "2001");
+        bookList.put(bookOne, true);
+        bookList.put(bookTwo, true);
+        Books books = new Books(bookList);
+        ReturnOption returnOption = new ReturnOption(consoleInputOutput, bookParser, books);
+
+        returnOption.obtainOptionResult();
+
+        verify(consoleInputOutput).getUserInput();
+    }
+}
