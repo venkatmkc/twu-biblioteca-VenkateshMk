92fa3fd86f214990a7f26d456c230defc462ab44
Venkatmkc <venkatmkc@gmail.com>
2015-06-16 19:32:46 +0530

    Added spec, unsuccessful book return should produce failure message

--- src/com/twu/biblioteca/NullBook.java
+++ src/com/twu/biblioteca/NullBook.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 
+import static com.twu.biblioteca.Messages.UNSUCCESSFUL_BOOK_RETURN;
 import static com.twu.biblioteca.Messages.UNSUCCESSFUL_CHECKOUT;
 
 public class NullBook extends Book{
@@ -9,5 +10,9 @@ public class NullBook extends Book{
 
     public String getCheckoutMessage() {
         return UNSUCCESSFUL_CHECKOUT;
+    }
+
+    public String getReturnMessage() {
+        return UNSUCCESSFUL_BOOK_RETURN;
     }
 }
--- test/com/twu/biblioteca/NullBookTest.java
+++ test/com/twu/biblioteca/NullBookTest.java
@@ -2,7 +2,9 @@ package com.twu.biblioteca;
 
 import org.junit.Test;
 
+import static com.twu.biblioteca.Messages.UNSUCCESSFUL_BOOK_RETURN;
 import static com.twu.biblioteca.Messages.UNSUCCESSFUL_CHECKOUT;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
@@ -14,5 +16,14 @@ public class NullBookTest {
         String actualStatus = nullBook.getCheckoutMessage();
 
         assertThat(actualStatus, is(UNSUCCESSFUL_CHECKOUT));
+    }
+
+    @Test
+    public void unsuccessfulBookReturnShouldProduceFailureMessage() {
+        NullBook nullBook = new NullBook();
+
+        String actualStatus = nullBook.getReturnMessage();
+
+        assertThat(actualStatus, is(UNSUCCESSFUL_BOOK_RETURN));
     }
 }
