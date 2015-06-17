3d8792b2f226e529b62de8c01258a55ca7513c39
Venkatmkc <venkatmkc@gmail.com>
2015-06-17 13:44:04 +0530

    Added spec, library number should be obtained from the user for login

---
+++ src/com/twu/biblioteca/Login.java
@@ -0,0 +1,19 @@
+package com.twu.biblioteca;
+
+import com.twu.biblioteca.io.ConsoleInputOutput;
+
+import java.util.HashSet;
+
+public class Login {
+    private HashSet<User> users;
+    private ConsoleInputOutput consoleInputOutput;
+
+    public Login(HashSet<User> users, ConsoleInputOutput consoleInputOutput) {
+        this.users = users;
+        this.consoleInputOutput = consoleInputOutput;
+    }
+
+    public void validate() {
+        String libraryNumber = consoleInputOutput.getUserInput();
+    }
+}
---
+++ test/com/twu/biblioteca/LoginTest.java
@@ -0,0 +1,39 @@
+package com.twu.biblioteca;
+
+import com.twu.biblioteca.io.ConsoleInputOutput;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class LoginTest {
+    @Mock
+    ConsoleInputOutput consoleInputOutput;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+    }
+
+    @Test
+    public void loginShouldGetLibraryNumberFromUser() {
+        HashSet<User> users = new HashSet<User>();
+        User userOne = new User("111-0000", "biblioteca");
+        User userTwo = new User("222-2222", "logmein");
+        users.add(userOne);
+        users.add(userTwo);
+        Login login = new Login(users, consoleInputOutput);
+
+        login.validate();
+
+        verify(consoleInputOutput).getUserInput();
+    }
+}
